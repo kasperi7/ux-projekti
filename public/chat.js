052f065eebc5dde680af9ae4d9681a9469da56c9
sossut <mikki.alakari@elisanet.fi>
2022-10-11 22:33:09 +0300

    chatti korjaus ja autoscroll

--- public/chat.js
+++ public/chat.js
@@ -13,30 +13,47 @@ document.querySelector("#msg-input").addEventListener("submit", (event) => {
 });
 
 document.querySelector("#join").addEventListener("submit", (event) => {
-  event.preventDefault();
   const username = document.getElementById("username");
   const room = document.querySelector('input[name="room"]:checked').value;
   socket.emit("join", username.value, room);
   username.value = "";
-  document.querySelector("#join").style.display = "";
-  document.querySelector("#msg-input").style.display = "";
-  document.querySelector("#log-out").style.display = "";
-  document.querySelector("#messages").style.display = "";
+  document.querySelector("#join").style.display = "none";
+  document.querySelector("#chat-room").style.display = "block";
+  document.querySelector("#msg-input").style.display = "block";
+  document.querySelector("#log-out").style.display = "block";
   roomName.innerHTML = "Liityit huoneeseen " + room;
+  event.preventDefault();
 });
 
 socket.on("chat message", (msg, username) => {
   const item = document.createElement("li");
   item.classList.add("bg-sandbrown", "p-3", "text-black", "w-fit", "m-5");
   item.innerHTML = `<b> ${username}: </b>` + msg;
-  document.getElementById("messages").appendChild(item);
-});
 
+  messages.appendChild(item);
+  autoScroll();
+});
+const autoScroll = () => {
+  const messages = document.getElementById("messages");
+  const newMessage = messages.lastElementChild;
+  const newMessageStyles = getComputedStyle(newMessage);
+  const newMessageMargin = parseInt(newMessageStyles.marginBottom);
+  const newMessageHeight = newMessage.offsetHeight + newMessageMargin;
+  const visibleHeight = messages.offsetHeight;
+  const containerHeight = messages.scrollHeight;
+  const scrollOffset = messages.scrollTop + visibleHeight;
+  if (containerHeight - newMessageHeight <= scrollOffset) {
+    messages.scrollTop = messages.scrollHeight;
+  }
+};
 document.querySelector("#log-out").addEventListener("click", (event) => {
   event.preventDefault();
   socket.emit("leave");
+  document.querySelector("#join").style.display = "block";
+  document.querySelector("#msg-input").style.display = "none";
+  document.querySelector("#log-out").style.display = "none";
+  document.querySelector("#chat-room").style.display = "none";
   messages.innerHTML = "";
-  location.reload();
   return false;
 });
 
